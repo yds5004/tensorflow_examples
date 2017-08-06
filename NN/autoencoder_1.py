f3b3fb4fd3316b59baf5fdce29fda8250b5e99ad
dsyoon <dosangyoon@gmail.com>
2017-08-07 03:01:45 +0900

    GAN 추가

--- NN/autoencoder_1.py
+++ NN/autoencoder_1.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 # Import MINST data
 from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets("rsc/MNIST_data", one_hot=True)
+mnist = input_data.read_data_sets("../resources", one_hot=True)
 
 
 # Parameters
@@ -14,7 +14,6 @@ learning_rate = 0.01
 training_epochs = 20
 batch_size = 256
 display_step = 1
-examples_to_show = 10
 
 # Network Parameters
 n_hidden_1 = 256 # 1st layer num features
@@ -25,6 +24,12 @@ n_input = 784 # MNIST data input (img shape: 28*28)
 # tf Graph input (only pictures)
 X = tf.placeholder("float", [None, n_input])
 
+# 인코더 레이어와 디코더 레이어의 가중치와 편향 변수를 설정합니다.
+# 다음과 같이 이어지는 레이어를 구성하기 위한 값들 입니다.
+#     (input -> encode -> decode -> output)
+# encode 의 아웃풋 크기를 입력값보다 작은 크기로 만들어 정보를 압축하여 특성을 뽑아내고,
+# decode 의 출력을 입력값과 동일한 크기를 갖도록하여 입력과 똑같은 아웃풋을 만들어 내도록 합니다.
+# 히든 레이어의 구성과 특성치을 뽑아내는 알고리즘을 변경하여 다양한 오토인코더를 만들 수 있습니다.
 weights = {
     'encoder_h1': tf.Variable(tf.random_normal([n_input,    n_hidden_1])),
     'encoder_h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2])),
@@ -59,8 +64,13 @@ def decoder(x):
     return layer_2
 
 
-# Construct model
+# sigmoid 함수를 이용해 신경망 레이어를 구성합니다.
+# sigmoid(X * W + b)
+# 인코더 레이어 구성
 encoder_op = encoder(X)
+
+# 디코더 레이어 구성
+# 이 디코더가 최종 모델이 됩니다.
 decoder_op = decoder(encoder_op)
 
 # Prediction
@@ -68,7 +78,8 @@ y_pred = decoder_op
 # Targets (Labels) are the input data.
 y_true = X
 
-# Define loss and optimizer, minimize the squared error
+# 디코더는 인풋과 최대한 같은 결과를 내야 하므로, 디코딩한 결과를 평가하기 위해
+# 입력 값인 X 값을 평가를 위한 실측 결과 값으로하여 decoder 와의 차이를 손실값으로 설정합니다.
 cost = tf.reduce_mean(tf.pow(y_true - y_pred, 2))
 optimizer = tf.train.RMSPropOptimizer(learning_rate).minimize(cost)
 
@@ -95,6 +106,13 @@ for epoch in range(training_epochs):
         print("Epoch:", '%04d' % (epoch+1), "cost=", "{:.9f}".format(c))
 
 print("Optimization Finished!")
+
+
+#########
+# 결과 확인
+# 입력값(위쪽)과 모델이 생성한 값(아래쪽)을 시각적으로 비교해봅니다.
+######
+examples_to_show = 10
 
 # Applying encode and decode over test set
 encode_decode = sess.run( y_pred, feed_dict={X: mnist.test.images[:examples_to_show]} )
